0f6d0eec38585fd7b75bd12236b4437e14c29764
Emilsivertsson <Emil.sivertsson@gmail.com>
2023-07-13 22:05:44 +0200

    first

--- src/main/java/org/CodeForPizza/Person.java
+++ src/main/java/org/CodeForPizza/Person.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 // this annotation is used to specify that this class is the parent class of the inheritance
 @Inheritance
 // discriminator column is used to differentiate between the subclasses
+//all wiil be saved into the same table, but the discriminator column will be used to differentiate between the subclasses
 @DiscriminatorColumn(name= "discriminator",discriminatorType = DiscriminatorType.STRING)
 // this value is used to differentiate between the subclasses
 @DiscriminatorValue("p")
